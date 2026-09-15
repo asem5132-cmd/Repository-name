6c8b8f697c19be466f54b05bf8d9c6e3d91661d7
asem5132-cmd <asem5132@gmail.com>
2026-09-15 19:44:57 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <meta name="google-site-verification" content="pzMTyxHkIb1pBXdxnzsz-usp3o7Sq-HvS_jbTYyih6I" />
+        
       </head>
       <body>{children}</body>
     </html>
